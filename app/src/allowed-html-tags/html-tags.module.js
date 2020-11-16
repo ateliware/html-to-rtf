245185c45cbcb7fc91e0dc8523850195de23a207
Victor Schena <39089725+victor-schena@users.noreply.github.com>
2020-11-16 17:32:55 -0300

    Fix/underline (#1)
    * Fixed italics.
    
    * Removed space after text
    
    * Removed console.log
    
    * Seted default font-size to 11pt
    
    * Added defchp to header
    
    * Fixed cell amount considering colspan.
    
    * Add new calculation to build tds correctly
    
    * Change flatMap to forEach
    
    * Rollback index.js
    
    * Fix center rtf tag definition
    
    * Fixed unit tests for table methods.
    
    * Removed ansi from rtf file header.
    
    Co-authored-by: Victor Hugo <victor.laynez@ateliware.com>
    Co-authored-by: victor-schena <victor.schena@ateliware.com>
    Co-authored-by: Lucas Manika Koeb <lkoeb186@gmail.com>

--- app/src/allowed-html-tags/html-tags.module.js
+++ app/src/allowed-html-tags/html-tags.module.js
@@ -13,7 +13,7 @@ module.exports = [
    },
    {
       opening: 'center',
-      openingRtf: '{\\pard\\qr',
+      openingRtf: '{\\pard\\qc',
       closing: '/center',
       closingRtf: '\\par}'
    },
@@ -25,7 +25,7 @@ module.exports = [
    },
    {
       opening: 'em',
-      openingRtf: '{\\b',
+      openingRtf: '{\\i',
       closing: '/em',
       closingRtf: '}'
    },
