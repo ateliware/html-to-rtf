2fbc6682b673b38418df2e6fe33a35804a2623e4
victor-schena <victor.schena@ateliware.com>
2020-11-18 13:46:28 -0300

    Added support to hidden text.

--- app/src/style/style.class.js
+++ app/src/style/style.class.js
@@ -4,7 +4,7 @@ const Color     = require('../color/color.class');
 const Alignment = require('../alignment/alignment.class');
 const FontSize  = require('../font-size/font-size.class');
 const AllowedStyleProperties = require('../allowed-style-properties/allowed-style-properties.class');
-
+const Display = require('../display/display.class');
 class Style {
   static getRtfReferenceColor(value) {
     return Color.getRtfReferenceColor(value);
@@ -22,26 +22,31 @@ class Style {
     return FontSize.getRtfFontSizeReference(value);
   }
 
+  static getRtfDisplayReference(value) {
+    return Display.getRtfDisplayContent(value);
+  }
+
   static getRtfReferencesInStyleProperty(styleValue) {
     if(styleValue == '')
       return undefined;
 
     let fictitiousTagWithTruthStyle = "<span style='"+styleValue+"'></span>";
     let listOfRtfReferences = '';
-    
+
     AllowedStyleProperties.getAllowedTags().forEach(value => {
       if($(fictitiousTagWithTruthStyle).css(value.propertyName) != undefined) {
         switch(value.propertyName) {
           case 'color': listOfRtfReferences       += this.getRtfReferenceColor($(fictitiousTagWithTruthStyle).css(value.propertyName)); break;
           case 'font-size': listOfRtfReferences   += this.getRtfFontSizeReference($(fictitiousTagWithTruthStyle).css(value.propertyName)); break;
-          case 'text-align': listOfRtfReferences  += this.getRtfAlignmentReference($(fictitiousTagWithTruthStyle).css(value.propertyName)); break;
+          case 'text-align': listOfRtfReferences += this.getRtfAlignmentReference($(fictitiousTagWithTruthStyle).css(value.propertyName)); break;
+          case 'display': listOfRtfReferences  += this.getRtfDisplayReference($(fictitiousTagWithTruthStyle).css(value.propertyName)); break;
         }
       }
     });
 
     if(listOfRtfReferences == '')
       return undefined;
-      
+
     return listOfRtfReferences;
   }
 }
