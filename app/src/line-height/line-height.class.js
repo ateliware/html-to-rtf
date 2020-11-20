837122e9557ad351a0ff18640c9e282e3a588b0d
Lucas Manika Koeb <lkoeb186@gmail.com>
2020-11-20 14:10:25 -0300

    Add normal to ignored line-height

--- app/src/line-height/line-height.class.js
+++ app/src/line-height/line-height.class.js
@@ -3,11 +3,12 @@ const LINE_HEIGHT_MULT = "\\slmult1"
 
 class LineHeight {
   static getRtfLineHeigthContent(value) {
-    if (value === "1")
+    if (value === "1" || value === "normal")
       return '';
 
     let convertedValue = parseFloat(value) * 240;
     return LINE_HEIGHT_OPENING + convertedValue + LINE_HEIGHT_MULT;
+
   }
 }
 
